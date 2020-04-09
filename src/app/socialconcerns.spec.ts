977bb16c43ba7d72c22d07291adb0ef518d86f38
Jerry Goodnough <jerry.goodnough@stormwoods.com>
2020-04-09 10:52:14 -0700

    Checkpoint work in progress

---
+++ src/app/socialconcerns.spec.ts
@@ -0,0 +1,7 @@
+import { Socialconcerns } from './socialconcerns';
+
+describe('Socialconcerns', () => {
+  it('should create an instance', () => {
+    expect(new Socialconcerns()).toBeTruthy();
+  });
+});
---
+++ src/app/socialconcerns.ts
@@ -0,0 +1,100 @@
+export class Socialconcerns {
+   fields = [
+     {
+       name: 'Food Security',
+       description: '',
+       fhirpath: '',
+       default: ''
+     },
+     {
+       name: 'Transportation Access',
+       description: '',
+       fhirpath: '',
+       default: ''
+     },
+     {
+       name: 'Housing Stability',
+       description: '',
+       fhirpath: '',
+       default: ''
+     },
+     {
+       name: 'Primary Language',
+       description: '',
+       fhirpath: 'From Demographics',
+       default: ''
+     },
+     {
+       name: 'Health Insurance Status/Type',
+       description: '',
+       fhirpath: '',
+       default: ''
+     },
+     {
+       name: 'History of Abuse',
+       description: '',
+       fhirpath: '',
+       default: ''
+     },
+     {
+       name: 'Smoking Status',
+       description: '',
+       fhirpath: '',
+       default: ''
+     },
+     {
+       name: 'Hea;th Literacy',
+       description: '',
+       fhirpath: '',
+       default: ''
+     },
+     {
+       name: 'Computer/Phone Access',
+       description: '',
+       fhirpath: '',
+       default: ''
+     },
+     {
+       name: 'ALcohol Abuse',
+       description: '',
+       fhirpath: '',
+       default: ''
+     },
+     {
+       name: 'Substance Abuse',
+       description: '',
+       fhirpath: '',
+       default: ''
+     },
+     {
+       name: 'Caregiver Characteristics',
+       description: '',
+       fhirpath: '',
+       default: ''
+     },
+     {
+       name: 'Characteristics of Home Environment',
+       description: '',
+       fhirpath: '',
+       default: ''
+     },
+     {
+       name: 'Employment Status',
+       description: '',
+       fhirpath: '',
+       default: ''
+     },
+     {
+       name: 'Education Level',
+       description: '',
+       fhirpath: '',
+       default: ''
+     },
+     {
+       name: 'Enviromental Conditions',
+       description: '',
+       fhirpath: '',
+       default: ''
+     },
+   ];
+}
