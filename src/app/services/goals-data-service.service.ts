b628e438327ae1df6610f2e7ff0ada0a8ba94d0b
Stephen Maier <stevemaier@rocketmail.com>
2020-09-18 10:21:59 -0700

    moved formatgoalTargetValue function from goals-data-service.service.ts to src/utility-functions.ts

--- src/app/services/goals-data-service.service.ts
+++ src/app/services/goals-data-service.service.ts
@@ -8,6 +8,7 @@ import {environment} from '../../environments/environment';
 import {GoalLists, GoalTarget, MccGoal, MccObservation} from '../generated-data-api';
 
 import {TargetValue} from '../datamodel/old/targetvalue';
+import {formatGoalTargetValue} from '../../utility-functions';
 
 @Injectable({
   providedIn: 'root'
@@ -69,7 +70,7 @@ export class GoalsDataService {
                 }
               }
 
-              [formattedTargetValue, rowHighlighted] = this.formatTargetValue(gt, mostRecentResultValue);
+              [formattedTargetValue, rowHighlighted] = formatGoalTargetValue(gt, mostRecentResultValue);
               const tv: TargetValue = {
                 measure: gt.measure.text,
                 date: observationDate, // todo: Get observation date when API is updated
@@ -100,125 +101,6 @@ export class GoalsDataService {
       tap(_ => this.log(`fetched MccObservation patientId=${patientId} code=${code}`)),
       catchError(this.handleError<MccObservation>(`getMostRecentObservationResult patientId=${patientId} code=${code}`))
     );
-  }
-
-
-  formatTargetValue(target: GoalTarget, mostRecentResultValue: string): any[] {
-    let formatted = 'Unknown Type: ';
-    let highlighted = false;
-    let rval = 0;
-    let qval = 0;
-    let highval = 0;
-    let lowval = 0;
-
-    rval = Number(mostRecentResultValue);
-    if (isNaN(rval)) {
-      rval = 0;
-    }
-
-    if (target.value !== undefined) {
-      formatted += ' ' + target.value.valueType;
-      switch (target.value.valueType) {
-        case 'String': {
-          formatted = target.value.stringValue;
-          break;
-        }
-        case 'Integer': {
-          formatted = target.value.integerValue.toString();
-          break;
-        }
-        case 'Boolean': {
-          formatted = String(target.value.booleanValue);
-          break;
-        }
-        case 'CodeableConcept': {
-          // todo:  formatTargetValue CodeableConcept
-          break;
-        }
-        case 'Quantity': {
-          formatted = target.value.quantityValue.comparator
-            + target.value.quantityValue.value.toString()
-            + ' ' + target.value.quantityValue.unit;
-          qval = Number(target.value.quantityValue.value);
-          if (!isNaN(qval)) {
-            if (target.value.quantityValue.comparator === '<') {
-              if (rval >= qval) {
-                highlighted = true;
-              }
-            }
-            if (target.value.quantityValue.comparator === '>') {
-              if (rval <= qval) {
-                highlighted = true;
-              }
-            }
-            if (target.value.quantityValue.comparator === '=') {
-              if (rval !== qval) {
-                highlighted = true;
-              }
-            }
-          }
-          break;
-        }
-        case 'Range': {
-          formatted = target.value.rangeValue.low.value
-            + ' - ' + target.value.rangeValue.high.value
-            + ' ' + target.value.rangeValue.high.unit;
-
-          highval = Number(target.value.rangeValue.high.value);
-          lowval = Number(target.value.rangeValue.low.value);
-          if (!isNaN(lowval) && !isNaN(highval)) {
-            if (rval < lowval || rval > highval) {
-              highlighted = true;
-            }
-          }
-          break;
-        }
-        case 'Ratio': {
-          // todo:  formatTargetValue Ratio
-          break;
-        }
-        case 'Period': {
-          // todo:  formatTargetValue Period
-          break;
-        }
-        case 'Date': {
-          // todo:  formatTargetValue Date
-          break;
-        }
-        case 'Time': {
-          // todo:  formatTargetValue Time
-          break;
-        }
-        case 'DateTime': {
-          // todo:  formatTargetValue DateTime
-          break;
-        }
-        case 'SampledData': {
-          // todo:  formatTargetValue SampledData
-          break;
-        }
-        case 'DurationValue': {
-          // todo:  formatTargetValue DurationValue
-          break;
-        }
-        case 'TimingValue': {
-          // todo:  formatTargetValue TimingValue
-          break;
-        }
-        case 'InstantValue': {
-          // todo:  formatTargetValue InstantValue
-          break;
-        }
-        case 'IdentifierValue': {
-          // todo:  formatTargetValue IdentifierValue
-          break;
-        }
-
-      }
-    }
-
-    return [formatted, highlighted];
-
   }
 
   /**
