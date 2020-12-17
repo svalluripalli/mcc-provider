b89968b2b0c71f6bd1f2b426b47a77f63d5b0389
Jerry Goodnough <jerry.goodnough@stormwoods.com>
2020-12-17 13:23:15 -0800

    Retrofit in Configuration Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  mccapiUrl: 'https://mcc-niddk-backend.wl.r.appspot.com',
-  clientId: '0950cf46-841c-466f-8bfb-19aa7d351b77'
+  mccapiUrl:  window["env"]["apiServer"] || 'https://mcc-niddk-backend.wl.r.appspot.com',
+  clientId:  window["env"]["clientId"] || '0950cf46-841c-466f-8bfb-19aa7d351b77'
 };
