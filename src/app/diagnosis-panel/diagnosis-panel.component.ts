2f4a7fa11547fa9f6b4bc8e7a4000b8cb2e96779
aschutzman-cognitive <75691772+aschutzman-cognitive@users.noreply.github.com>
2021-04-07 15:54:27 -0400

    Most sections toggle-able

--- src/app/diagnosis-panel/diagnosis-panel.component.ts
+++ src/app/diagnosis-panel/diagnosis-panel.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Constants } from '../common/constants';
+import featureToggling from "../../assets/json/feature-toggling.json";
 
 declare var window: any;
 
@@ -9,7 +10,8 @@ declare var window: any;
   styleUrls: ['./diagnosis-panel.component.css']
 })
 export class DiagnosisPanelComponent implements OnInit {
-
+  featureToggling: any = featureToggling;
+  
   constructor() { }
 
   ngOnInit(): void {
--- src/app/health-status/health-status.component.ts
+++ src/app/health-status/health-status.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import featureToggling from "../../assets/json/feature-toggling.json";
 
 @Component({
   selector: 'app-health-status',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./health-status.component.css']
 })
 export class HealthStatusComponent implements OnInit {
+  featureToggling: any = featureToggling;
 
   constructor() { }
 
