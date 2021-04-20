4b479f10bff99f2a6f9461cb7f6d23c30edc842d
aschutzman-cognitive <75691772+aschutzman-cognitive@users.noreply.github.com>
2021-04-20 10:18:04 -0400

    More logging.

--- src/app/services/medication.service.ts
+++ src/app/services/medication.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {MessageService} from './message.service';
-import {Observable, of} from 'rxjs';
-import {MedicationSummaryList} from '../generated-data-api';
-import {environment} from '../../environments/environment';
-import {catchError, tap} from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { MessageService } from './message.service';
+import { Observable, of } from 'rxjs';
+import { MedicationSummaryList } from '../generated-data-api';
+import { environment } from '../../environments/environment';
+import { catchError, tap } from 'rxjs/operators';
 
 
 @Injectable({
@@ -18,30 +18,30 @@ export class MedicationService {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
 
-  constructor(private http: HttpClient,  private messageService: MessageService) { }
+  constructor(private http: HttpClient, private messageService: MessageService) { }
 
   /** GET medicationsummary by subject id and careplan Id.  Will 404 if id not found */
   getMedicationSummaryBySubjectAndCareplan(subjectId: string, careplanId: string): Observable<MedicationSummaryList> {
     // const url = `${environment.mccapiUrl}${this.listURL}?subject=${subjectId}&careplan=${careplanId}`;
     const url = `${environment.mccapiUrl}${this.summaryURL}?subject=${subjectId}&careplan=${careplanId}`;
     return this.http.get<MedicationSummaryList>(url, this.httpOptions).pipe(
-      tap(_ => this.log(`fetched Medication Lists id=${subjectId}, careplan=${careplanId}`)),
+      tap((_) => { this.log(`fetched Medication Lists id=${subjectId}, careplan=${careplanId}`); console.log("Fetched Medications", _); }),
       catchError(this.handleError<MedicationSummaryList>(`getContacts id=${subjectId}, careplan=${careplanId}`))
     );
   }
 
   /** GET medicationsummary by subject id. Will 404 if id not found */
 
-/*
-  getMedicationsSummary(subjectId: string, careplanId: string): Observable<MedicationSummary[]> {
-    // const url = `${environment.mccapiUrl}${this.listURL}?subject=${subjectId}&careplan=${careplanId}`;
-    const url = `${environment.mccapiUrl}${this.listURL}?subject=${subjectId}&careplan=${careplanId}`;
-    return this.http.get<MedicationSummary[]>(url, this.httpOptions).pipe(
-      tap(_ => this.log(`fetched MedicationSummary id=${subjectId}, careplan=${careplanId}`)),
-      catchError(this.handleError<MedicationSummary[]>(`getContacts id=${subjectId}, careplan=${careplanId}`))
-    );
-  }
-*/
+  /*
+    getMedicationsSummary(subjectId: string, careplanId: string): Observable<MedicationSummary[]> {
+      // const url = `${environment.mccapiUrl}${this.listURL}?subject=${subjectId}&careplan=${careplanId}`;
+      const url = `${environment.mccapiUrl}${this.listURL}?subject=${subjectId}&careplan=${careplanId}`;
+      return this.http.get<MedicationSummary[]>(url, this.httpOptions).pipe(
+        tap(_ => this.log(`fetched MedicationSummary id=${subjectId}, careplan=${careplanId}`)),
+        catchError(this.handleError<MedicationSummary[]>(`getContacts id=${subjectId}, careplan=${careplanId}`))
+      );
+    }
+  */
 
   /**
    * Handle Http operation that failed.
